b2347feb46e805059d388b98ee2ce7c00eccf49f
msncoder <msncoder1999@gmail.com>
2025-03-18 00:59:23 +0500

    create notes model in api app

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.ForeignKey(User,on_delete=models.CASCADE,related_name="notes")
+
+    def __str__(self):
+        return self.title
