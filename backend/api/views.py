a650107c73389187d5f43a38268e32bd66cc84ad
msncoder <msncoder1999@gmail.com>
2025-03-18 01:50:45 +0500

    add notes create and delete view and make urls.py file in api app

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializer import UserSerializer
+from .models import Notes
+from .serializer import UserSerializer,NoteSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
 # Create your views here.
+
+class NotesListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notes.objects.filter(author = user)
+    
+    def perform_create(self,serializer):
+        if serializer.is_valid():
+            serializer.save(author = self.request.user)
+        else:
+            print(serializer.error)
+
+class NotesDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Notes.objects.filter(author = user)
+
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
