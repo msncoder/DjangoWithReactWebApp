06969feb94d06fc63cfe94b0d40e5d88e5308340
msncoder <msncoder1999@gmail.com>
2025-03-18 01:15:30 +0500

    recorrection in notes Model in api app

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Notes(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.ForeignKey(User,on_delete=models.CASCADE,related_name="notes")
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    created_at = models.DateTimeField(auto_now_add=True)        
+    
     def __str__(self):
         return self.title
