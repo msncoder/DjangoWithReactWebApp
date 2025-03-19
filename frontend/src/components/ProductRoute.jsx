e5efc357f51ff8656f7d70b84a942cb4287f93bd
msncoder <msncoder1999@gmail.com>
2025-03-20 01:46:52 +0500

    create ProductRoute inside component

---
+++ frontend/src/components/ProductRoute.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { jwtDecode, JWTDecode } from "jwt-decode";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constraints";
+import { Navigate } from "react-router-dom";
+function ProductRoute({ children }) {
+  const [isAuthorized, SetIsAuthorize] = useState(null);
+
+  useEffect(() => {
+    auth().catch(() => SetIsAuthorize(false));
+  }, []);
+
+  const refreshtoken = async () => {
+    const refreshtoken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const res = api.post("/api/token/refresh/", { refresh, refreshtoken });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        SetIsAuthorize(true);
+      } else {
+        SetIsAuthorize(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const auth = async () => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      SetIsAuthorize(false);
+      return;
+    }
+    const decode = jwtDecode(token);
+    const tokenExpiration = decode.exp;
+    const now = Date.now() / 1000;
+
+    if (tokenExpiration < now) {
+      await refreshtoken();
+    } else {
+      SetIsAuthorize(true);
+    }
+  };
+
+  if (isAuthorized == null) {
+    return <div>Loading...</div>;
+  }
+  return isAuthorized ? children : <Navigate to="/login/" />;
+}
+
+export default ProductRoute;
