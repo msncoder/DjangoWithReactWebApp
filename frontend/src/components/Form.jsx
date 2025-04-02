93e2fb2ea019549620db4ed2dc41845608cf4013
msncoder <msncoder1999@gmail.com>
2025-04-03 00:51:10 +0500

    Revert "add css for homepage and loading indicator for login page"
    This reverts commit 1d85e9e9ebb51b3f7059843e670bcfad866ecef0.

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,7 +3,6 @@ import api from "../api";
 import { Navigate, useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constraints";
 import "../styles/Form.css";
-import Loadingindicator from "./Loadingindicator";
 function Form({ route, method }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -51,8 +50,6 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
-
-      {loading && <Loadingindicator />}
 
       <button className="form-button" type="submit">
         {name}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import api from "../api";
-import Note from "../components/Note";
-import "../styles/Home.css";
-
 function Home() {
   const [notes, setNotes] = useState([]);
   const [content, setContent] = useState("");
@@ -29,9 +26,9 @@ function Home() {
       .then((res) => {
         if (res.status === 204) alert("Notes Deleted");
         else alert("Failed to delete Note");
-        getNotes();
       })
       .catch((err) => alert(err));
+    getNotes();
   };
 
   const createNotes = (e) => {
@@ -41,50 +38,12 @@ function Home() {
       .then((res) => {
         if (res.status === 201) alert("Notes created");
         else alert("Failed to create Note");
-        getNotes();
       })
       .catch((err) => alert(err));
+    getNotes();
   };
 
-  return (
-    <>
-      <div>
-        <h2>Notes</h2>
-
-        {notes.map((note) => (
-          <Note note={note} onDelete={deleteNotes} />
-        ))}
-      </div>
-      <h2>Create Notes</h2>
-      <form onSubmit={createNotes}>
-        <label htmlFor="title">Title:</label>
-        <br />
-        <input
-          type="text"
-          id="title"
-          name="title"
-          required
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
-
-        <label htmlFor="content">Content:</label>
-        <br />
-        <textarea
-          name="content"
-          id="content"
-          required
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-
-        <br />
-
-        <input type="submit" value="submit" />
-      </form>
-      ;
-    </>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
